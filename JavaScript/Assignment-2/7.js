dc629e3d6992ed8c2f964b5d8eff95bd4335f7c0
VIDYA <93605465+THIRTHALA@users.noreply.github.com>
2021-12-06 15:42:11 +0530

    Create 7.js

---
+++ JavaScript/Assignment-2/7.js
@@ -0,0 +1,11 @@
+const square = num => num*num;
+function find(arr, f) {
+    var ele = [];
+    for (let i = 0; i < arr.length; i++) {
+        ele[i] = f(arr[i]);
+    }
+    return ele;
+}
+
+// printing functions as a string concatnation
+"My find() conatins this: " + find;
